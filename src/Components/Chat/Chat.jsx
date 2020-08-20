b005d4a8a13a92437b068c53204fef9fe7f73310
Manojkumar M <manojkumar.m8497@gmail.com>
2020-08-20 12:22:17 +0530

    UI finished

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -4,17 +4,25 @@ import { Avatar, IconButton } from "@material-ui/core";
 import AttachFile from "@material-ui/icons/AttachFile";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 // CSS
 import "./Chat.css";
 
 function Chat() {
   // State
   const [avatarName, setAvatarName] = useState("");
+  const [message, setMessage] = useState("");
   // Component Life cycle hooks
   useEffect(() => {
     const random = Math.floor(Math.random() * 10);
     setAvatarName(random);
   }, []);
+  // Handle submit function
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log(message);
+  };
   return (
     <div className="chat">
       {/* Chat header */}
@@ -39,9 +47,39 @@ function Chat() {
         </div>
       </div>
       {/* Chat Messages */}
-      <div className="chat__body"></div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Manojkumar</span>
+          <span className="chat__content">
+            Many more happy return of the day
+          </span>
+          <span className="chat__timestamp">8:20 AM</span>
+        </p>
+        <p className={`chat__message ${true && "chat__receiver"}`}>
+          <span className="chat__name">GaneshKumar</span>
+          <span className="chat__content">Thank you so much</span>
+          <span className="chat__timestamp">8:25 AM</span>
+        </p>
+      </div>
       {/* Chat Footer */}
-      <div className="chat__footer"></div>
+      <div className="chat__footer">
+        {/* emoji keyboard */}
+        <InsertEmoticonIcon />
+        {/* Type a message */}
+        <form>
+          <input
+            type="text"
+            placeholder="Type a message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message
+          </button>
+        </form>
+        {/* Mic icon */}
+        <MicIcon />
+      </div>
     </div>
   );
 }
